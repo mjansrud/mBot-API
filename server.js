58dc47082f2367e30f146898335d2580356948b2
mjansrud <morten@founder.no>
2017-05-27 14:16:41 +0200

    Removed comments

--- server.js
+++ server.js
@@ -27,43 +27,12 @@ const authCheck = jwt({
     algorithms: ['RS256']
 });
 
-app.get('/api/jokes/food', (req, res) => {
-
-    let foodJokes = [
-        {
-            id: 99991,
-            joke: "When Chuck Norris was a baby, he didn't suck his mother's breast. His mother served him whiskey, straight out of the bottle."
-        },
-        {
-            id: 99992,
-            joke: 'When Chuck Norris makes a burrito, its main ingredient is real toes.'
-        },
-        {
-            id: 99993,
-            joke: 'Chuck Norris eats steak for every single meal. Most times he forgets to kill the cow.'
-        },
-        {
-            id: 99994,
-            joke: "Chuck Norris doesn't believe in ravioli. He stuffs a live turtle with beef and smothers it in pig's blood."
-        },
-        {
-            id: 99995,
-            joke: "Chuck Norris recently had the idea to sell his urine as a canned beverage. We know this beverage as Red Bull."
-        },
-        {
-            id: 99996,
-            joke: 'When Chuck Norris goes to out to eat, he orders a whole chicken, but he only eats its soul.'
-        }
-    ];
-    res.json(foodJokes);
-})
-
-
 /*
 
     Poloniex
 
  */
+
 let poloniex = new Poloniex(process.env.NODE_ENV_POLONIEX_KEY, process.env.NODE_ENV_POLONIEX_SECRET, { socketTimeout: 15000 });
 
 app.get('/api/ticker', (req,res) => {
@@ -80,11 +49,6 @@ app.get('/api/currencies', (req,res) => {
 
 app.get('/api/trades/:pair', authCheck, (req,res) => {
 
-    console.log("Pair" + req.query.pair);
-
-    let tickers = [];
-    res.json(tickers);
-
 })
 
 app.get('/api/chart/:pair/:period/:start/:end', authCheck, (req,res) => {
@@ -95,10 +59,6 @@ app.get('/api/chart/:pair/:period/:start/:end', authCheck, (req,res) => {
 
 app.get('/api/sessions', authCheck, (req,res) => {
 
-    console.log("Pair" + req.query.pair);
-
-    let tickers = [];
-    res.json(tickers);
 })
 
 
