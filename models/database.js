7812e09336ed06dae8212ced0aa1a9ebac2b585e
mjansrud <morten@founder.no>
2017-05-28 22:35:25 +0200

    Added env. on database

--- models/database.js
+++ models/database.js
@@ -3,9 +3,13 @@
  Call this manually once
 
  */
+
+//Make environmental variables available
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('mbot', 'jansrud', 'password', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.NODE_ENV_DATABASE_NAME, process.env.NODE_ENV_DATABASE_USERNAME, process.env.NODE_ENV_DATABASE_PASSWORD, {
+    host: process.env.NODE_ENV_DATABASE_HOST,
     dialect: 'postgres',
 
     pool: {
@@ -14,7 +18,17 @@ const sequelize = new Sequelize('mbot', 'jansrud', 'password', {
         idle: 10000
     }
 
-});
+}); 
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
 
 const User = sequelize.define('user', {
     username: Sequelize.STRING,
@@ -30,15 +44,7 @@ User.sync({force: true}).then(() => {
     });
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
 
-User.findAll().then(users => {
-    console.log(users)
+User.findAll().then(user => {
+    console.log(user)
 })
